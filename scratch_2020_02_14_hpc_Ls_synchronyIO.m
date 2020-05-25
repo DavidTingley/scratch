efbf7951f40f1d4c30bb1fd98f9c1d1e1d7566ff
David Tingley <davidtingley2@gmail.com>
2020-05-25 21:02:00 +0200

    /

--- scratch_2020_02_14_hpc_Ls_synchronyIO.m
+++ scratch_2020_02_14_hpc_Ls_synchronyIO.m
@@ -1,17 +1,16 @@
-% folders = dir('*201*');
-% 
-% for f=1:length(folders)
-%     cd(folders(f).name)
-%     if exist([folders(f).name '.spikes.cellinfo.mat']) & exist([folders(f).name '.CA1Ripples.events.mat'])
+folders = dir('*201*');
 
-        f=1;
+for f=1:length(folders)
+    cd(folders(f).name)
+    if exist([folders(f).name '.spikes.cellinfo.mat']) & exist([folders(f).name '.CA1Ripples.events.mat'])
+
+%         f=1;
         sessionInfo = bz_getSessionInfo;
         load([sessionInfo.FileName '.behavior.mat'])
         spikes = bz_GetSpikes('noprompts',true);
-        spkMat = bz_SpktToSpkmat(spikes,'binSize',.025,'overlap',1);
-        spkMat_large = bz_SpktToSpkmat(spikes,'binSize',.1,'overlap',4);
-%         spkMat = bz_SpktToSpkmat(spikes,'binSize',.1,'overlap',1);
-%         spkMat_large = bz_SpktToSpkmat(spikes,'binSize',.4,'overlap',4);
+        spkMat = bz_SpktToSpkmat(spikes,'binSize',.1,'overlap',4);
+%        spkMat = bz_SpktToSpkmat(spikes,'binSize',.1,'overlap',1);
+%        spkMat_large = bz_SpktToSpkmat(spikes,'binSize',.4,'overlap',4);
         hpc = find(strcmp(spikes.region ,'hpc') | strcmp(spikes.region ,'ca1'));
         latS = find(strcmp(spikes.region ,'ls'));
 %         reg(f) = 0;
@@ -36,8 +35,8 @@
 
             [pks locs] = findpeaks(-phase);
             % cut by power thresh
-            thresh = mean(td_pow)-std(td_pow);
-            idx = find(td_pow(locs)>thresh);
+            powThresh(f) = mean(td_pow)+std(td_pow)*2;
+            idx = find(td_pow(locs)>powThresh(f));
             cycles = lfp.timestamps(locs(idx));
             % cut by behavior
             id = find(InIntervals(cycles,[behavior.timestamps(1) behavior.timestamps(end)]));
@@ -49,11 +48,12 @@
                 [a b]=min(abs(cycles(c)-behavior.timestamps));
                 vels(c)=behavior.velocity(b);            
             end
-    %         cycles(vels<20)=[]; clear vels
+%             cycles(vels<10)=[]; clear vels
             velocities{f} = vels; clear vels
 
             powers{f} = td_pow(round(cycles*1250));   
-
+%             
+            %% get theta cycle times
             thetaCycleCount = zeros(ceil(lfp.timestamps(end)*1000/25),1);
             thetaCycleCount(ceil(cycles*1000/25)) = 1;
             %% get ripple times
@@ -69,7 +69,7 @@
                 id = FindInInterval(spkIndices,[cycles(cyc)-thresh cycles(cyc)+thresh]);
                 if ~isempty(id)
                     temp(cyc) = diff(id)+1;
-                    s = ceil((spkIndices(id(1):id(2))-cycles(cyc)+.06)*1000);
+                    s = round((spkIndices(id(1):id(2))-cycles(cyc)+.06)*1000);
                     tempPETH(cyc,:) = hist(s,1:120);
                 else
                     temp(cyc) = 0;
@@ -90,7 +90,7 @@
                 id = FindInInterval(spkIndices,[ripples.peaks(rip)-thresh ripples.peaks(rip)+thresh]);
                 if ~isempty(id)
                     temp(rip) = diff(id)+1;
-                    s = ceil((spkIndices(id(1):id(2))-ripples.peaks(rip)+.06)*1000);
+                    s = round((spkIndices(id(1):id(2))-ripples.peaks(rip)+.06)*1000);
                     tempPETH(rip,:) = hist(s,1:120);
                 else
                     temp(rip) = 0;
@@ -122,7 +122,7 @@
             end
             hpcRates_z = nanmean(spkMat.dataZ(:,hpc)');
             hpcRates = nanmean(spkMat.data(:,hpc)');
-            hpcPercentActive = nansum(spkMat_large.data(:,hpc)')./length(hpc);
+            hpcPercentActive = nansum(spkMat.data(:,hpc)')./length(hpc);
             lsRates_z = nanmean(spkMat.dataZ(:,latS)');
             lsRates = nanmean(spkMat.data(:,latS)');
             
@@ -157,36 +157,36 @@
             nHPC_cells(f) = length(hpc);
             nLS_cells(f) = length(latS);
             
-%             subplot(4,2,1)
-%             imagesc(rr)
-%             subplot(4,2,2)
-%             imagesc(rz)
-%             subplot(4,2,3)
-%             imagesc(zr)
-%             subplot(4,2,4)
-%             plot(nanmean(zr(nHPC_cells>15,:)))
-%             subplot(4,2,5)
-%             plot(mean(zscore(ripHisto(nHPC_cells>15,:),[],2)))
-%             hold on
-%             plot(mean(zscore(thetaHisto(nHPC_cells>15,:),[],2)))
-%             hold off            
-%             subplot(4,2,6)
-%             plot(mean(zscore(ripHisto_z(nHPC_cells>15,:),[],2)))
-%             hold on
-%             plot(mean(zscore(thetaHisto_z(nHPC_cells>15,:),[],2)))
-%             hold off
-%             subplot(4,2,7)
-%             plot(mean((ripPercentHisto(nHPC_cells>15,:))))
-%             hold on
-%             plot(mean((thetaPercentHisto(nHPC_cells>15,:))))
-%             hold off
-%             pause(.1)
+            subplot(4,2,1)
+            imagesc(rr)
+            subplot(4,2,2)
+            imagesc(rz)
+            subplot(4,2,3)
+            imagesc(zr)
+            subplot(4,2,4)
+            plot(nanmean(zr(nHPC_cells>15,:)))
+            subplot(4,2,5)
+            plot(mean(zscore(ripHisto(nHPC_cells>15,:),[],2)))
+            hold on
+            plot(mean(zscore(thetaHisto(nHPC_cells>15,:),[],2)))
+            hold off            
+            subplot(4,2,6)
+            plot(mean(zscore(ripHisto_z(nHPC_cells>15,:),[],2)))
+            hold on
+            plot(mean(zscore(thetaHisto_z(nHPC_cells>15,:),[],2)))
+            hold off
+            subplot(4,2,7)
+            plot(mean((ripPercentHisto(nHPC_cells>15,:))))
+            hold on
+            plot(mean((thetaPercentHisto(nHPC_cells>15,:))))
+            hold off
+            pause(.1)
         end
     
-%     end
-%     cd D:\datasets\lsDataset
-% end
-FileName = sessionInfo.FileName;
-clear lfp spikes spkMat* td_pow filt filt_lo phase ripples behavior sessionInfo
-save([FileName '.synchronyAnalysis.mat'],'-v7.3')
+    end
+    cd D:\datasets\lsDataset
+end
+% FileName = sessionInfo.FileName;
+% clear lfp spikes spkMat* td_pow filt filt_lo phase ripples behavior sessionInfo
+% save([FileName '.synchronyAnalysis.mat'],'-v7.3')
 %% need to find all theta cycles and ripples and plot histograms over HPC synchrony percentiles
---
+++ scratch_2020_05_25_lsFR_synchrony.m
@@ -0,0 +1,88 @@
+% folders = dir('*201*');
+
+% for f=1:length(folders)
+%     cd(folders(f).name)
+%     if exist([folders(f).name '.spikes.cellinfo.mat']) & exist([folders(f).name '.CA1Ripples.events.mat'])
+
+        f=1;
+        sessionInfo = bz_getSessionInfo;
+        load([sessionInfo.FileName '.behavior.mat'])
+        spikes = bz_GetSpikes('noprompts',true);
+        spkMat = bz_SpktToSpkmat(spikes,'binSize',.1,'overlap',4);
+%        spkMat = bz_SpktToSpkmat(spikes,'binSize',.1,'overlap',1);
+%        spkMat_large = bz_SpktToSpkmat(spikes,'binSize',.4,'overlap',4);
+        hpc = find(strcmp(spikes.region ,'hpc') | strcmp(spikes.region ,'ca1'));
+        latS = find(strcmp(spikes.region ,'ls'));
+%         reg(f) = 0;
+        if isempty(hpc)
+            hpc = find(strcmp(spikes.region ,'ca3'));
+%             reg(f) = 1;
+        end
+
+       
+        %% carry on
+        if ~isempty(hpc) & ~isempty(latS)
+            for i=1:length(spikes.times)
+                spkMat.dataZ(:,i) = zscore(spkMat.data(:,i));
+            end
+            hpcRates_z = nanmean(spkMat.dataZ(:,hpc)');
+            hpcRates = nanmean(spkMat.data(:,hpc)');
+            hpcPercentActive = nansum(spkMat.data(:,hpc)')./length(hpc);
+            lsRates_z = nanmean(spkMat.dataZ(:,latS)');
+            lsRates = nanmean(spkMat.data(:,latS)');
+            
+            hpcRates_smooth = fastrms(hpcRates,24);
+            lsRates_smooth = fastrms(lsRates,24);
+            hpcRates_smooth_z = fastrms(hpcRates_z,24);
+            lsRates_smooth_z = fastrms(lsRates_z,24);
+          
+            for i=1:95
+                thresh_low = prctile(hpcRates_smooth,i-1);
+                thresh_hi = prctile(hpcRates_smooth,i+5);
+                idx = find(hpcRates_smooth>thresh_low & hpcRates_smooth<thresh_hi);
+                rr(f,i) = nanmean(lsRates_smooth(idx));
+                rz(f,i) = nanmean(lsRates_smooth_z(idx));
+                
+                thresh_low = prctile(hpcRates_smooth_z,i-1);
+                thresh_hi = prctile(hpcRates_smooth_z,i+5);
+                idx = find(hpcRates_smooth_z>thresh_low & hpcRates_smooth_z<thresh_hi);
+                zz(f,i) = nanmean(lsRates_smooth_z(idx));
+                zr(f,i) = nanmean(lsRates_smooth(idx));
+                
+            end
+            nHPC_cells(f) = length(hpc);
+            nLS_cells(f) = length(latS);
+            
+%             subplot(4,2,1)
+%             imagesc(rr)
+%             subplot(4,2,2)
+%             imagesc(rz)
+%             subplot(4,2,3)
+%             imagesc(zr)
+%             subplot(4,2,4)
+%             plot(nanmean(zr(nHPC_cells>15,:)))
+%             subplot(4,2,5)
+%             plot(mean(zscore(ripHisto(nHPC_cells>15,:),[],2)))
+%             hold on
+%             plot(mean(zscore(thetaHisto(nHPC_cells>15,:),[],2)))
+%             hold off            
+%             subplot(4,2,6)
+%             plot(mean(zscore(ripHisto_z(nHPC_cells>15,:),[],2)))
+%             hold on
+%             plot(mean(zscore(thetaHisto_z(nHPC_cells>15,:),[],2)))
+%             hold off
+%             subplot(4,2,7)
+%             plot(mean((ripPercentHisto(nHPC_cells>15,:))))
+%             hold on
+%             plot(mean((thetaPercentHisto(nHPC_cells>15,:))))
+%             hold off
+%             pause(.1)
+        end
+    
+%     end
+%     cd D:\datasets\lsDataset
+% end
+FileName = sessionInfo.FileName;
+clear lfp spikes spkMat* td_pow filt filt_lo phase ripples behavior sessionInfo
+save([FileName '.synchronyAnalysis.mat'],'-v7.3')
+%% need to find all theta cycles and ripples and plot histograms over HPC synchrony percentiles
