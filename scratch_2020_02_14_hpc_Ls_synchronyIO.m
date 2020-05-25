b4a101aae98c204ca3ababdd58a14f3fd4f40fe4
David Tingley <davidtingley2@gmail.com>
2020-05-25 12:09:12 +0200

    /

--- scratch_2020_02_14_hpc_Ls_synchronyIO.m
+++ scratch_2020_02_14_hpc_Ls_synchronyIO.m
@@ -26,71 +26,86 @@
         end
         %% get theta cycle times
         lfp = bz_GetLFP(lfpChan);
-        [b a] = butter(4,[5/625 12/625],'bandpass');
-        filt = filtfilt(b,a,double(lfp.data));
-        [b a] = butter(4,[4/625],'low');
-        filt_lo = filtfilt(b,a,double(lfp.data));
-        td_pow = fastrms(filt,125)./fastrms(filt_lo,1250);
-        phase = angle(hilbert(filt));
-        
-        [pks locs] = findpeaks(-phase);
-        % cut by power thresh
-        thresh = mean(td_pow)-std(td_pow);
-        idx = find(td_pow(locs)>thresh);
-        cycles = lfp.timestamps(locs(idx));
-        % cut by behavior
-        id = find(InIntervals(cycles,[behavior.timestamps(1) behavior.timestamps(end)]));
-        cycles = cycles(id);
-        behavior.velocity(end+1)=behavior.velocity(end);
-        
-        vels=behavior.velocity(ceil((cycles-behavior.timestamps(1))*round(behavior.samplingRate)));
-%         parfor c =1:length(cycles)
-%             [a b]=min(abs(cycles(c)-behavior.timestamps));
-%             vels(c)=behavior.velocity(b);            
-%         end
-%         cycles(vels<20)=[]; clear vels
-        velocities{f} = vels; clear vels
-   
-        powers{f} = td_pow(round(cycles*1250));   
-        
-        thetaCycleCount = zeros(ceil(lfp.timestamps(end)*1000/25),1);
-        thetaCycleCount(ceil(cycles*1000/25)) = 1;
-        %% get ripple times
-        load([sessionInfo.FileName '.CA1Ripples.events.mat'])
-        rippleCount = zeros(ceil(lfp.timestamps(end)*1000/25),1);
-        rippleCount(ceil(ripples.peaks*1000/25)) = 1;
-        %% spikes per event analysis
-        thresh = .06;
-        spkIndices = spikes.spindices(ismember(spikes.spindices(:,2),hpc));
-        spkIndices_ls = spikes.spindices(ismember(spikes.spindices(:,2),latS));
-        tempPETH = zeros(length(cycles),120);
-        parfor cyc = 1:length(cycles)
-            id = FindInInterval(spkIndices,[cycles(cyc)-thresh cycles(cyc)+thresh]);
-            temp(cyc) = diff(id)+1;
-            s = ceil((spkIndices(id(1):id(2))-cycles(cyc)+.06)*1000);
-            tempPETH(cyc,:) = hist(s,1:120);
-            if ~isempty(spkIndices_ls)
-                temp2(cyc) = diff(FindInInterval(spkIndices_ls,[cycles(cyc)-thresh cycles(cyc)+thresh]))+1;
-            end
-        end
-        thetaCycPETH{f} = tempPETH; clear tempPETH
-        spksPerThetaCyc{f} = temp; clear temp
-        spksPerThetaCyc_ls{f} = temp2; clear temp2
-        
-        tempPETH = zeros(length(ripples.peaks),120);
-        parfor rip =1:length(ripples.peaks)
-            id = FindInInterval(spkIndices,[ripples.peaks(rip)-thresh ripples.peaks(rip)+thresh]);
-            temp(rip) = diff(id)+1;
-            s = ceil((spkIndices(id(1):id(2))-ripples.peaks(rip)+.06)*1000);
-            tempPETH(rip,:) = hist(s,1:120);
-            if ~isempty(spkIndices_ls)
-                temp2(rip) = diff(FindInInterval(spkIndices_ls,[ripples.peaks(rip)-thresh ripples.peaks(rip)+thresh]))+1;
-            end
-        end
-        ripPETH{f} = tempPETH; clear tempPETH
-        spksPerRipple{f} = temp; clear temp
-        spksPerRipple_ls{f} = temp2; clear temp2
+        if exist([sessionInfo.FileName '.CA1Ripples.events.mat'])
+            [b a] = butter(4,[5/625 12/625],'bandpass');
+            filt = filtfilt(b,a,double(lfp.data));
+            [b a] = butter(4,[4/625],'low');
+            filt_lo = filtfilt(b,a,double(lfp.data));
+            td_pow = fastrms(filt,125)./fastrms(filt_lo,1250);
+            phase = angle(hilbert(filt));
 
+            [pks locs] = findpeaks(-phase);
+            % cut by power thresh
+            thresh = mean(td_pow)-std(td_pow);
+            idx = find(td_pow(locs)>thresh);
+            cycles = lfp.timestamps(locs(idx));
+            % cut by behavior
+            id = find(InIntervals(cycles,[behavior.timestamps(1) behavior.timestamps(end)]));
+            cycles = cycles(id);
+            behavior.velocity(end+1)=behavior.velocity(end);
+
+            vels=behavior.velocity(ceil((cycles-behavior.timestamps(1))*round(behavior.samplingRate)));
+    %         parfor c =1:length(cycles)
+    %             [a b]=min(abs(cycles(c)-behavior.timestamps));
+    %             vels(c)=behavior.velocity(b);            
+    %         end
+    %         cycles(vels<20)=[]; clear vels
+            velocities{f} = vels; clear vels
+
+            powers{f} = td_pow(round(cycles*1250));   
+
+            thetaCycleCount = zeros(ceil(lfp.timestamps(end)*1000/25),1);
+            thetaCycleCount(ceil(cycles*1000/25)) = 1;
+            %% get ripple times
+            load([sessionInfo.FileName '.CA1Ripples.events.mat'])
+            rippleCount = zeros(ceil(lfp.timestamps(end)*1000/25),1);
+            rippleCount(ceil(ripples.peaks*1000/25)) = 1;
+            %% spikes per event analysis
+            thresh = .06;
+            spkIndices = spikes.spindices(ismember(spikes.spindices(:,2),hpc));
+            spkIndices_ls = spikes.spindices(ismember(spikes.spindices(:,2),latS));
+            tempPETH = zeros(length(cycles),120);
+            parfor cyc = 1:length(cycles)
+                id = FindInInterval(spkIndices,[cycles(cyc)-thresh cycles(cyc)+thresh]);
+                if ~isempty(id)
+                    temp(cyc) = diff(id)+1;
+                else
+                    temp(cyc) = 0;
+                end
+                s = ceil((spkIndices(id(1):id(2))-cycles(cyc)+.06)*1000);
+                tempPETH(cyc,:) = hist(s,1:120);
+                if ~isempty(spkIndices_ls) & ~isempty(FindInInterval(spkIndices_ls,[cycles(cyc)-thresh cycles(cyc)+thresh]))
+                    temp2(cyc) = diff(FindInInterval(spkIndices_ls,[cycles(cyc)-thresh cycles(cyc)+thresh]))+1;
+                end
+            end
+            thetaCycPETH{f} = tempPETH; clear tempPETH
+            spksPerThetaCyc{f} = temp; clear temp
+            if exist('temp2')
+            spksPerThetaCyc_ls{f} = temp2; clear temp2
+            end
+            tempPETH = zeros(length(ripples.peaks),120);
+            parfor rip =1:length(ripples.peaks)
+                id = FindInInterval(spkIndices,[ripples.peaks(rip)-thresh ripples.peaks(rip)+thresh]);
+                if ~isempty(id)
+                    temp(rip) = diff(id)+1;
+                else
+                    temp(rip) = 0;
+                end
+                s = ceil((spkIndices(id(1):id(2))-ripples.peaks(rip)+.06)*1000);
+                tempPETH(rip,:) = hist(s,1:120);
+                if ~isempty(spkIndices_ls) & ~isempty(FindInInterval(spkIndices_ls,[cycles(cyc)-thresh cycles(cyc)+thresh]))
+                    temp2(rip) = diff(FindInInterval(spkIndices_ls,[ripples.peaks(rip)-thresh ripples.peaks(rip)+thresh]))+1;
+                end
+            end
+            ripPETH{f} = tempPETH; clear tempPETH
+            spksPerRipple{f} = temp; clear temp
+            if exist('temp2')
+            spksPerRipple_ls{f} = temp2; clear temp2
+            end
+        else
+            rippleCount = nan(ceil(lfp.timestamps(end)*1000/25),1);
+            thetaCycleCount = nan(ceil(lfp.timestamps(end)*1000/25),1);
+        end
         %% carry on
         if ~isempty(hpc) & ~isempty(latS)
             for i=1:length(spikes.times)
