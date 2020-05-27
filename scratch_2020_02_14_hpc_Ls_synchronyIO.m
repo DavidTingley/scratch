2e510d1484f619c6cd4d6f2b82bb7cf91cde4fd4
David Tingley <davidtingley2@gmail.com>
2020-05-27 23:09:02 +0200

    Update scratch_2020_02_14_hpc_Ls_synchronyIO.m

--- scratch_2020_02_14_hpc_Ls_synchronyIO.m
+++ scratch_2020_02_14_hpc_Ls_synchronyIO.m
@@ -131,11 +131,11 @@
             for i=1:length(spikes.times)
                 spkMat.dataZ(:,i) = zscore(spkMat.data(:,i));
                 r = length(spikes.times{i})./lfp.timestamps(end);
-%                 if r > 5 & ismember(i,hpc) % toss those pesky interneurons
-%                     spkMat.data(:,i) = nan;
-%                     spkMat.dataZ(:,i) = nan;
-%                     toss = toss+1;
-%                 end
+                if r > 3 & ismember(i,hpc) % toss those pesky interneurons
+                    spkMat.data(:,i) = nan;
+                    spkMat.dataZ(:,i) = nan;
+                    toss = toss+1;
+                end
             end
             hpcCounts = nansum(spkMat.data(:,hpc)'>0);
             hpcRates_z = nanmean(spkMat.dataZ(:,hpc)');
