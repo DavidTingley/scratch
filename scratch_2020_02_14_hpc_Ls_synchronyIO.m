2cd749017aa7b66e9a08fe4da9c622a3d3cf3f99
David Tingley <davidtingley2@gmail.com>
2020-06-25 17:26:31 -0700

    Update scratch_2020_02_14_hpc_Ls_synchronyIO.m

--- scratch_2020_02_14_hpc_Ls_synchronyIO.m
+++ scratch_2020_02_14_hpc_Ls_synchronyIO.m
@@ -147,7 +147,7 @@
             hpcCounts_smooth = fastrms(hpcCounts,24);
             hpcRates_smooth = fastrms(hpcRates,24);
             lsRates_smooth = fastrms(lsRates,24);
-            hpcRates_smooth_z = fastrms(hpcRates_z,24)-fastrms(hpcRates_z,1200);
+            hpcRates_smooth_z = fastrms(hpcRates_z,24);%-fastrms(hpcRates_z,1200);
             lsRates_smooth_z = fastrms(lsRates_z,24);
             for i=1:100
                idx = find(hpcPercentActive>i/100 & hpcPercentActive>(i+10)/100);
